60b4777f7bed34e5f6ffc584e3214647b383cc27
ahmetson <ahmetson@zoho.com>
2021-08-21 18:40:17 +0700

    Seapad: Add Seapad Tier contract and its migration

---
+++ migrations/13_seapad.js
@@ -0,0 +1,27 @@
+var Tier = artifacts.require("./SeapadTier.sol");
+
+// The SeapadTier (Tier) contract constructor parameters
+// address _crowns, address _claimVerifier, uint256[3] memory _fees
+var Crowns = artifacts.require("./CrownsToken.sol");
+let claimVerifier = "";
+let fees = [
+    web3.utils.toWei("1", "ether"),     // Tier 0
+    web3.utils.toWei("10", "ether"),    // Tier 1    
+    web3.utils.toWei("50", "ether"),    // Tier 2
+    web3.utils.toWei("100", "ether"),   // Tier 3
+];
+
+async function getAccount(id) {
+    let accounts = await web3.eth.getAccounts();
+    return accounts[id];
+}
+
+module.exports = async function(deployer, network) {
+    claimVerifier = await getAccount();
+
+    if (network == "development") {
+        await deployer.deploy(Tier, Crowns.address, claimVerifier, fees);
+        
+        console.log("Deployed SeacapeTier address: " + Tier.address);
+    } 
+};
